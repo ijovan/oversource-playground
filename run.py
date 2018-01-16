8e2d5ec0b97e1a9e52ee5e1fab1c3d3df25abbe7
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2018-01-16 21:22:52 +0100

    Tweaking

--- run.py
+++ run.py
@@ -9,16 +9,16 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn import metrics
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from sklearn.model_selection import train_test_split
 from questions import Questions
 
 
 def fit(classifier, texts, tags):
-    train_texts = texts[:len(texts)//2]
-    test_texts = texts[len(texts)//2:]
-    train_tags = tags[:len(tags)//2]
-    test_tags = tags[len(tags)//2:]
+    train_texts, test_texts, train_tags, test_tags = \
+            train_test_split(texts, tags, test_size=0.8)
 
-    clf = Pipeline([('vect', CountVectorizer()),
+    clf = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2),
+                                             stop_words='english')),
                    ('tfidf', TfidfTransformer()),
                    ('clf', classifier)])
 
@@ -34,8 +34,8 @@ def print_top_words(model, feature_names, n_top_words, topics):
             if value == topic_idx:
                 match.append(key)
         print("---> Topic #" + str(topic_idx) + ": " + ", ".join(match))
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+        print(", ".join([feature_names[i]
+                         for i in topic.argsort()[:-n_top_words - 1:-1]]))
     print()
 
 
@@ -68,7 +68,7 @@ def languages_sentiment(questions, languages):
         print("{0}: {1}".format(language, sentiment(texts)))
 
 
-N_COMPONENTS = 16
+N_COMPONENTS = 20
 
 
 def lda(questions, languages):
@@ -80,10 +80,11 @@ def lda(questions, languages):
             topics[language][i] = 0
 
     tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
-                                       max_features=1000,
+                                       max_features=2000,
                                        stop_words='english')
     tfidf = tfidf_vectorizer.fit_transform(questions.texts())
-    lda = LatentDirichletAllocation(n_components=N_COMPONENTS, max_iter=5,
+    lda = LatentDirichletAllocation(n_components=N_COMPONENTS,
+                                    max_iter=10,
                                     learning_method='online',
                                     learning_offset=50.,
                                     random_state=0, verbose=1)
@@ -108,18 +109,33 @@ def lda(questions, languages):
             if value[i] > 0:
                 value[i] = value[i] / counts[i]
 
-    print(topics)
     final_topics = {}
     for key, value in topics.items():
         final_topics[key] = int(numpy.argmax(list(value.values())))
 
-    print_top_words(lda, tfidf_feature_names, 20, final_topics)
+    print_top_words(lda, tfidf_feature_names, 15, final_topics)
+
+
+LIMIT = 800
+
+
+def classify_even(questions, languages):
+    texts = []
+    tags = []
+    for lang in languages:
+        texts += questions.for_language(lang).texts()[:LIMIT]
+        tags += questions.for_language(lang).tags()[:LIMIT]
+    classify(texts, tags)
 
 
 languages = json.loads(open("../languages.json", "r").read())
 questions = Questions()
 
+######################################################################
+
 # classify(questions.texts(), questions.tags())
+
+# classify_even(questions, languages)
 
 # languages_sentiment(questions, languages)
 
