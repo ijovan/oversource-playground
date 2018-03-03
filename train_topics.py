f2118f3e3641f44941a95244df4d6e6618526105
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2018-03-03 17:27:53 +0100

    Use normal distributions for LDA mapping

--- train_topics.py
+++ train_topics.py
@@ -3,49 +3,57 @@ from questions import Questions
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from scipy.spatial import distance
+from scipy.stats import multivariate_normal
 from sklearn import metrics
 
 
 TRAIN_PERCENTAGE = 0.5
 
+NGRAM_RANGE = (1, 1)
 MAX_FEATURES = 3000
 MIN_DF = 1e-4
 TOKEN_PATTERN = "c[#\+]{1,2}|[!~][=]{1,2}|[:\-\+]{2}|[{}$@]|[()/]{2}|[A-Za-z]+"
 
-N_COMPONENTS = 20
-MAX_ITER = 5
+N_COMPONENTS = 10
+MAX_ITER = 3
 
 
-def map_tags_components(text_components, tags):
-    tag_components, tag_counts = {}, {}
+def map_tag_distributions(text_components, tags):
+    tag_components = {}
 
     for tag in tags:
-        tag_components[tag] = [0] * len(text_components[0])
-        tag_counts[tag] = 0
+        tag_components[tag] = []
 
     for index, components in enumerate(text_components):
         tag = train_tags[index]
-        tag_components[tag] = numpy.add(tag_components[tag], components)
-        tag_counts[tag] += 1
+        tag_components[tag].append(components)
+
+    tag_distributions = {}
 
     for tag in tags:
-        tag_components[tag] = \
-            numpy.divide(tag_components[tag], tag_counts[tag])
+        means, variances = [], []
 
-    return tag_components
+        for component in numpy.transpose(tag_components[tag]):
+            means.append(numpy.mean(component))
+            variances.append(numpy.var(component))
+
+        tag_distributions[tag] = {'means': means, 'variances': variances}
+
+    return tag_distributions
 
 
-def predict(text_components, tag_components):
+def predict(text_components, tag_distributions):
     predicted_tags = []
 
     for row in text_components:
-        distances = {}
+        pdfs = {}
 
         for tag in tags:
-            distances[tag] = distance.euclidean(row, tag_components[tag])
+            distribution = tag_distributions[tag]
+            means, variances = distribution['means'], distribution['variances']
+            pdfs[tag] = multivariate_normal.pdf(row, means, variances)
 
-        predicted_tag = min(distances, key=distances.get)
+        predicted_tag = max(pdfs, key=pdfs.get)
         predicted_tags.append(predicted_tag)
 
     return predicted_tags
@@ -59,7 +67,7 @@ train_texts, test_texts, train_tags, test_tags = train_test_split(
 )
 
 vectorizer = TfidfVectorizer(
-    ngram_range=(1, 1), stop_words='english', token_pattern=TOKEN_PATTERN,
+    ngram_range=NGRAM_RANGE, stop_words='english', token_pattern=TOKEN_PATTERN,
     analyzer="word", min_df=MIN_DF, max_features=MAX_FEATURES
 )
 
@@ -72,11 +80,11 @@ lda = LatentDirichletAllocation(
 
 train_text_components = lda.fit_transform(train_text_vectors)
 
-tag_components = map_tags_components(train_text_components, tags)
+tag_distributions = map_tag_distributions(train_text_components, tags)
 
 test_text_vectors = vectorizer.transform(test_texts)
 test_text_components = lda.transform(test_text_vectors)
 
-predicted_tags = predict(test_text_components, tag_components)
+predicted_tags = predict(test_text_components, tag_distributions)
 
 print(metrics.classification_report(test_tags, predicted_tags))
